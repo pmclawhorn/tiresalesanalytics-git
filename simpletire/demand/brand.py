3816ee4202c82ca3f764132accfd696c1889d33a
pmclawhorn <pmclawhorn@crimson.ua.edu>
2020-02-04 01:55:42 -0600

    Fixed file size limit
    Former-commit-id: ccdc7db0b140950c731a139b6021ec183e5cd156

--- simpletire/demand/brand.py
+++ simpletire/demand/brand.py
@@ -16,7 +16,7 @@ import seaborn as sns; sns.set()
 # Filter the Data to Necessary Columns, Remove Extraneous Data
 
 # FIXME if you are not Pierce then this path will need to be changed (lazy i will fix later)
-jan26_raw = pd.read_csv(r'/Users/piercemclawhorn/om597/simpletire-git/simpletire/OrderItemMargin-01-26.csv')
+jan26_raw = pd.read_csv(r'/Users/piercemclawhorn/om597/data/OrderItemMargin-01-26.csv')
 jan26 = jan26_raw.loc[:, ['Source', 'Created', 'ProductID', 'Quantity', 'Cost', 'Unit_Cost', 'Price', 'Unit_Price', 'Ext_Sales', 'Ext_Cost', 'Brand', 'Sub_Type', 'Line', 'Admin_Ship_Est']]
 jan26 = jan26.loc[~(jan26['Source'] == "BulkOrders")]  # Remove bulk orders
 # These Brands are selected from the top 10% of brands as computed in profit/brand.py
--- simpletire/demand/subtype.py
+++ simpletire/demand/subtype.py
@@ -17,7 +17,7 @@ import seaborn as sns; sns.set()
 # Filter the Data to Necessary Columns, Remove Extraneous Data
 
 # FIXME if you are not Pierce then this path will need to be changed (lazy i will fix later)
-jan26_raw = pd.read_csv(r'/Users/piercemclawhorn/om597/simpletire-git/simpletire/OrderItemMargin-01-26.csv')
+jan26_raw = pd.read_csv(r'/Users/piercemclawhorn/om597/data/OrderItemMargin-01-26.csv')
 jan26 = jan26_raw.loc[:, ['Source', 'Created', 'ProductID', 'Quantity', 'Cost', 'Unit_Cost', 'Price', 'Unit_Price', 'Ext_Sales', 'Ext_Cost', 'Brand', 'Sub_Type', 'Line', 'Admin_Ship_Est']]
 jan26 = jan26.loc[~(jan26['Source'] == "BulkOrders")]  # Remove bulk orders
 # jan26 = jan26.loc[jan26['Sub_Type'].isin(['ATV/UTV', 'Commercial', 'Farm', 'Golf', 'Lawn & Garden', 'Industrial',  'Passenger', 'Light Truck', 'Trailer'])]
