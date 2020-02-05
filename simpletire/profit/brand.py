74f10be5e597a1597bd760b6a5aa1e4906992f43
pmclawhorn <pmclawhorn@crimson.ua.edu>
2020-02-04 19:04:13 -0600

    Polished all scripts in profit package. Populated reports with profitability report. Added 2-way analysis

--- simpletire/profit/brand.py
+++ simpletire/profit/brand.py
@@ -3,45 +3,48 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
-import h5py
-import numpy as np
-
-# Hierarchy is Brand -> Subtype -> Line
 
 # Filter the Data to Necessary Columns, Remove Extraneous Data
-
-# FIXME if you are not Pierce then this path will need to be changed (lazy i will fix later)
+# TODO if you are not Pierce then you will need to change the file path
 jan26_raw = pd.read_csv(r'/Users/piercemclawhorn/om597/data/OrderItemMargin-01-26.csv')
-jan26 = jan26_raw.loc[:, ['Source', 'Created', 'ProductID', 'Quantity', 'Cost', 'Unit_Cost', 'Price', 'Unit_Price', 'Ext_Sales', 'Ext_Cost', 'Brand', 'Sub_Type', 'Line', 'Admin_Ship_Est']]
+jan26 = jan26_raw.loc[:, ['Source', 'Created', 'ProductID', 'Quantity', 'Cost', 'Unit_Cost', 'Price',
+                          'Unit_Price', 'Ext_Sales', 'Ext_Cost', 'Brand', 'Sub_Type', 'Line', 'Admin_Ship_Est'
+                          ]]
 jan26 = jan26.loc[~(jan26['Source'] == "BulkOrders")]  # Remove bulk orders
 
-# Compute Columns for profit, include shipping cost
+
+# Compute Column for profit, include shipping cost
 jan26['Net_Profit'] = ((jan26['Ext_Sales'] - jan26['Ext_Cost']) - jan26['Admin_Ship_Est'])
 
-n = 10
 
-# Hierarchy: Brand -> Subtype -> Line -> SKU
-
-# BRAND ANALYSIS
-brand = jan26.loc[:, ['ProductID', 'Quantity', 'Cost', 'Unit_Cost', 'Price', 'Unit_Price', 'Ext_Sales', 'Ext_Cost', 'Net_Profit', 'Brand']]
+# Brand Analysis - Aggregate by Brand and Sort by Profit (sum)
+brand = jan26.loc[:, ['ProductID', 'Quantity', 'Cost', 'Unit_Cost', 'Price', 'Unit_Price', 'Ext_Sales',
+                      'Ext_Cost', 'Net_Profit', 'Brand'
+                      ]]
 brand = brand.sort_values(['Net_Profit'], ascending=[False])
-# Aggregate by Brand
-brand_group = brand.groupby('Brand').agg({'Net_Profit': ['sum','mean', 'min', 'max']}).reset_index()
-# Sort by Profit (sum)
+brand_group = brand.groupby('Brand').agg({'Net_Profit': ['sum', 'mean']}).reset_index()
 brand_group = brand_group.sort_values([('Net_Profit', 'sum')], ascending=False)
 
-brand_group_top = brand_group.head(int(len(brand_group) * (n/100)))
-brand_group_bottom = brand_group.tail(int(len(brand_group) * (n/100)))
 
+# Get the top n% of brands
+n = 10
+brand_group_top = brand_group.head(int(len(brand_group) * (n/100)))
+# brand_group_bottom = brand_group.tail(int(len(brand_group) * (n/100)))
 print("\nBRAND AGGREGATE\n")
 print("Top 10% of Brands by Profit\n")
 print(brand_group_top)
+# print(brand_group_bottom)
 
-brand_plot = pd.DataFrame()
-brand_plot['Profit'] = brand_group_top.loc[:, ('Net_Profit', 'sum')]
-brand_plot['Brand'] = brand_group_top.loc[:, ['Brand']]
 
+# Output file to simpletire/reports/
 brand_group_top.to_csv(r'/Users/piercemclawhorn/om597/simpletire-git/simpletire/reports/brandprofit.csv', encoding='utf-8', index=True)
+
+
+# Plot TODO
+#brand_plot = pd.DataFrame()
+#brand_plot['Profit'] = brand_group_top.loc[:, ('Net_Profit', 'sum')]
+#brand_plot['Brand'] = brand_group_top.loc[:, ['Brand']]
+
 #print(brand_plot)
 #brand_plot.plot('Brand', 'Profit', figsize=(15, 6))
 #plt.show()
--- simpletire/profit/subtype.py
+++ simpletire/profit/subtype.py
@@ -3,35 +3,33 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
-import h5py
-import numpy as np
 
-pd.set_option("display.max_columns", 999)
-pd.set_option("display.max_rows", 999)
+# Filter the Data to Necessary Columns, Remove Extraneous Data
+# TODO if you are not Pierce then you will need to change the file path
+jan26_raw = pd.read_csv(r'/Users/piercemclawhorn/om597/data/OrderItemMargin-01-26.csv')
+jan26 = jan26_raw.loc[:, ['Source', 'Created', 'ProductID', 'Quantity', 'Cost', 'Unit_Cost', 'Price',
+                          'Unit_Price', 'Ext_Sales', 'Ext_Cost', 'Brand', 'Sub_Type', 'Line', 'Admin_Ship_Est'
+                          ]]
+jan26 = jan26.loc[~(jan26['Source'] == "BulkOrders")]  # Remove bulk orders
 
-dec6_raw = pd.read_csv("OrderItemMargin.csv")
-#print(dec6_raw.head())
 
-dec6_condensed = dec6_raw.loc[:, ['Source', 'Created', 'ProductID', 'Quantity', 'Cost', 'Unit_Cost', 'Price', 'Unit_Price', 'Ext_Sales', 'Ext_Cost', 'Brand', 'Sub_Type', 'Line', 'Admin_Ship_Est']]
-dec6_condensed['Net_Profit'] = dec6_condensed['Ext_Sales'] - dec6_condensed['Ext_Cost']
-#print(dec6_condensed.head())
+# Compute Column for profit, include shipping cost
+jan26['Net_Profit'] = ((jan26['Ext_Sales'] - jan26['Ext_Cost']) - jan26['Admin_Ship_Est'])
 
-n = 10
 
-# Hierarchy: Brand -> Subtype -> Line -> SKU
-
-# SUBTYPE ANALYSIS
-subtype = dec6_condensed.loc[:, ['ProductID', 'Quantity', 'Cost', 'Unit_Cost', 'Price', 'Unit_Price', 'Ext_Sales', 'Ext_Cost', 'Net_Profit', 'Sub_Type']]
+# SubType Analysis - Aggregate by SubType and Sort by Profit (sum)
+subtype = jan26.loc[:, ['ProductID', 'Quantity', 'Cost', 'Unit_Cost', 'Price', 'Unit_Price', 'Ext_Sales',
+                      'Ext_Cost', 'Net_Profit', 'Sub_Type'
+                    ]]
 subtype = subtype.sort_values(['Ext_Sales'], ascending=[False])
-# Aggregate by Sub_Type
-subtype_group = subtype.groupby('Sub_Type').agg({'Net_Profit': ['sum','mean', 'min', 'max']}).reset_index()
-# Sort by Quantity (sum)
+subtype_group = subtype.groupby('Sub_Type').agg({'Net_Profit': ['sum','mean']}).reset_index()
 subtype_group = subtype_group.sort_values([('Net_Profit', 'sum')], ascending=False)
+
 
 print("\nSUBTYPE AGGREGATE\n")
 print(subtype_group)
 
-subtype_group.plot(figsize=(15, 6))
-plt.show()
+#subtype_group.plot(figsize=(15, 6))
+#plt.show()
 
 subtype_group.to_csv(r'/Users/piercemclawhorn/om597/simpletire-git/simpletire/reports/subtypeprofit.csv', encoding='utf-8', index=True)
