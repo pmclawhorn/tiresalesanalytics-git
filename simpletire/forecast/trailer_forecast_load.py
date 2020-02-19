dd1e19bbaad86a37b1772d6013541fb7e33875c5
pmclawhorn <pmclawhorn@crimson.ua.edu>
2020-02-19 15:03:22 -0600

    Fix package-level run configuration

--- simpletire/forecast/trailer_forecast_load.py
+++ simpletire/forecast/trailer_forecast_load.py
@@ -11,9 +11,7 @@ import statsmodels.api as sm
 import matplotlib
 from pylab import rcParams
 import seaborn as sns; sns.set()
-from forecast/trailer_forecast_load.py import subtype_result
 
-'''
 # Hierarchy is Brand -> Subtype -> Line
 
 # Filter the Data to Necessary Columns, Remove Extraneous Data
@@ -38,53 +36,4 @@ subtype_demand = subtype_demand.groupby('Sub_Type').resample('W-Mon', on='Create
 subtype_result = subtype_demand.groupby(['Created', 'Sub_Type'])['Quantity'].sum().reset_index().\
     pivot(index='Created', columns='Sub_Type', values='Quantity').resample('W-Mon', label='left', closed='left').\
     asfreq().fillna(0)
-# subtype_result.to_csv(r'/Users/piercemclawhorn/om597/simpletire-git/simpletire/reports/trailerdemand.csv',\
-#                      encoding='utf-8', index=True)
-'''
-# This represents Sales by Week for each subtype
-# Plot Subtype
-# print(subtype_result)
-# subtype_result.plot(figsize=(30, 6))
-    # fig, ax = plt.subplots()
-    # ax.plot('Created', 'Trailer', data=subtype_result)
-# plt.xlabel('Month')
-# plt.ylabel('Quantity Sold')
-# plt.title('Number of Trailer Tires Sold by Week: 2018-2019')
-    # plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
-    # plt.xticks(np.arange(0, 500, 6000))
-    # plt.yticks(np.arange(0, 11, 1))
-# plt.grid(True)
-# plt.show()
 
-# Plot Variance
-print("VARIANCE:\n")
-print(subtype_result.var())
-# subvar = subtype_result.var()
-# subvar.plot(figsize = (15, 6))
-# plt.show()
-
-# Plot StdDev
-print("STANDARD DEVIATION:\n")
-print(np.std(subtype_result))
-
-# FORECASTING
-
-# subtype_series = subtype_result.reset_index()
-subtype_series = subtype_result
-# subtype_series = subtype_series.loc[:, ['Passenger']]
-print(subtype_series)
-
-# Show Graph for Seasonal Decomposition
-
-# plt.rcParams['lines.linewidth'] = 3.0
-plt.rcParams['figure.figsize'] = 18, 8
-plt.rcParams['figure.facecolor'] = 'white'
-plt.rcParams['axes.prop_cycle'] = matplotlib.cycler(color=["#ff6832", "#000000", "0.7"])
-decomposition = sm.tsa.seasonal_decompose(subtype_series, model='multiplicative')
-# print(decomposition)
-figure = decomposition.plot()
-fig = plt.figure()
-# ax = plt.gca()
-plt.title('Trailer Tires - Seasonal Decomposition by Quantity Sold - 01/01/18-01-26-20')
-plt.grid(True)
-plt.show()
